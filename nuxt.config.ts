9eeadd6f70167048577d31ac7aa86ce29dd1c436
rbantug <ronbantug22@gmail.com>
2023-08-28 23:35:47 +0800

    added new page and component. Also updated nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    ssr: false,
     devtools: { enabled: true },
+    experimental: {
+        payloadExtraction: false
+    },
     runtimeConfig: {
         secret: process.env.TEST_ONE,
         public: {
